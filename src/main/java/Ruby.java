9ebd533c3e301c1e89f3f1de435caad47913780d
JiahaoGan <u3585924@connect.hku.hk>
2024-01-25 23:11:39 +0800

    Rename as Ruby, add greet and exit function

---
+++ src/main/java/Ruby.java
@@ -0,0 +1,15 @@
+public class Ruby {
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+
+    private static void greet(){
+        System.out.println("Hi, I am here. Greets from Ruby.");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void exit(){
+        System.out.println("Bye. Always feels good work with you.");
+    }
+}
